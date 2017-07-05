abc0bdd20c6215ff2fc741175b19362ee622705b
04raghuramvugs <raghuramvugs@gmail.com>
2017-07-05 20:00:03 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,16 @@
+var mongodb = require("mongodb");
+var express = require("express");
+var mongoClient = mongodb.MongoClient;
+var app = express();
+
+app.get("/mongodb",function (req,res) {
+    mongoClient.connect("mongodb://raghuram:raghuram@ds121222.mlab.com:21222/emp1" , function (err,db) {
+        var collection = db.collection("e2");
+        collection.find().toArray(function (err, doc) {
+            res.send(doc);
+        });
+    });
+});
+
+app.listen(8080);
+console.log("Server Started on Port No.8080");
